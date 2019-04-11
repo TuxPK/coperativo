c74b46aa35221ee8488817f156743a0c7dbe7441
Marcos Gauderth <marcosxr8@gmail.com>
2019-04-11 12:35:56 -0300

    importacao inicial

---
+++ copere-conosco.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Coperativo</title>
+</head>
+<body>
+<div id="interface">
+    <!--Cabeçalho da página.-->
+    <header id="cabecalho">
+        <img id="logo" src="_imagens/cropped-cooperativo-logo-5.jpg">
+        <nav id="menu">
+            <ul>
+                <li><a href="index.php">Inicial</a></li>
+                <li><a href="sobre.html">Sobre</a></li>
+                <li><a href="contato.html">Contato</a></li>
+                <li><a href="Copere-conosco.php">Copere conosco</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!--Informações sobre o Corporativo-->
+    <section id="corpo">
+        <article id="noticias">
+            <header id="cabnoticias">
+                <h1>Login</h1>
+            </header>
+            <form >
+        </article>
+    </section>
+    <!--Área reservada para publicidade-->
+    <aside id="lateral">
+
+        Espaço reservado para publicidade.
+
+    </aside>
+    <footer id="rodape">
+
+        <p>Copyright &copy; 2019 - by Marcos Gauderth<br/>
+    </footer>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Coperativo</title>
+</head>
+<body>
+<div id="interface">
+    <!--Cabeçalho da página.-->
+    <header id="cabecalho">
+        <img id="logo" src="_imagens/cropped-cooperativo-logo-5.jpg">
+        <nav id="menu">
+            <ul>
+                <li><a href="index.php">Inicial</a></li>
+                <li><a href="sobre.html">Sobre</a></li>
+                <li><a href="contato.html">Contato</a></li>
+                <li><a href="Copere-conosco.php">Copere conosco</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!--Noticias e artigos.-->
+    <section id="corpo">
+        <article id="noticias">
+            <header id="cabnoticias">
+                <h1>Titulo Noticia</h1>
+                <h3>4 DE ABRIL DE 2019	~ DEIXE UM COMENTÁRIO</h3>
+            </header>
+            <p>
+                Esperei até ontem para poder ter um parecer sobre esta reforma que está sendo difundida como a salvação do Brasil. Ontem pois foi quando o ministro Guedes foi defender a reforma na câmara. Em Primeiro lugar parece que a reforma foi feita as pressas e nem a base de apoio do governo concorda com ela.
+            </p>
+        </article>
+        <article id="noticias">
+            <header id="cabnoticias">
+                <h1>Titulo Noticia</h1>
+                <h3>4 DE ABRIL DE 2019	~ DEIXE UM COMENTÁRIO</h3>
+            </header>
+            <p>
+                Esperei até ontem para poder ter um parecer sobre esta reforma que está sendo difundida como a salvação do Brasil. Ontem pois foi quando o ministro Guedes foi defender a reforma na câmara. Em Primeiro lugar parece que a reforma foi feita as pressas e nem a base de apoio do governo concorda com ela.
+            </p>
+        </article>
+    </section>
+    <!--Área reservada para publicidade-->
+    <aside id="lateral">
+
+        Espaço reservado para publicidade.
+
+    </aside>
+    <footer id="rodape">
+
+        <p>Copyright &copy; 2019 - by Marcos Gauderth<br/>
+    </footer>
+</div>
+</body>
+</html>
